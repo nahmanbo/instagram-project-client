dcbf92ae2dfa85bf7046235878a7b10c162b730d
nahmanbo <nhmanbo2003@gmail.com>
2025-09-07 17:41:58 +0300

    Creating a header and basic design

---
+++ src/components/application-layout/Header.tsx
@@ -0,0 +1,6 @@
+import type { PropsWithChildren } from "react";
+import ".../styles/application-layout/header.css";
+
+export default function Header({ children }: PropsWithChildren) {
+  return (<header className="header">{children}</header>);
+}
