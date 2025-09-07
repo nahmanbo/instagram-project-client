fdb62d0c3f2daa8b1e5c20475dd9c67e9de7a079
nahmanbo <nhmanbo2003@gmail.com>
2025-09-07 17:54:16 +0300

    I created a logo, post tab, post board, title, and ensured clean code and dynamic design.

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,7 @@ function App() {
       <Post name={postData[0].name} img={postData[0].imjUrl} description={postData[0].description} dateISO={postData[0].dateISO}/>
       <Post name={postData[0].name} img={postData[0].imjUrl} description={postData[0].description} dateISO={postData[0].dateISO}/>
       <Post name={postData[0].name} img={postData[0].imjUrl} description={postData[0].description} dateISO={postData[0].dateISO}/>
-
-      </PostBoard>
+    </PostBoard>
     </>
   )
 }
