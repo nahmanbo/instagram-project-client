afeda9dd3719b463e1a1f3bdb6039032bddd8967
nahmanbo <nhmanbo2003@gmail.com>
2025-09-07 11:43:21 +0300

    Adding a basic logo design and importing the logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
-
+import linkodkodLogo from "./assets/images/linkodkodLogo.jpeg"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={linkodkodLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
