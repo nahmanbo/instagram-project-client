88fab67ecc635d1bacf4aa6d89c3f32209f95403
nahmanbo <nhmanbo2003@gmail.com>
2025-09-10 11:16:18 +0300

    Adding a feature: Clicking on a post takes you to the post page.

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,8 +2,8 @@ import LikeButton from "./LikeButton";
 import Logo from "./Logo";
 
 import "../styles/post.css";
+import { Link } from "react-router";
 const BASE_URL = "http://localhost:1212"; 
-
 
 export interface PostI {
   id: number;
@@ -14,9 +14,9 @@ export interface PostI {
 }
 
 
-export default function Post({ name, img, description, dateISO }: PostI) {
+export default function Post({id, name, img, description, dateISO }: PostI) {
   return (
-      <div className="post-container">
+      <Link to={`/post/${id}`} className="post-container" >
           <div className="img-container">
               <Logo></Logo>
               <img src={`${BASE_URL}/${img}`}  className="post-img" />
@@ -30,6 +30,6 @@ export default function Post({ name, img, description, dateISO }: PostI) {
               <LikeButton/>
           </div>
 
-      </div>
+      </Link>
   );
 }
